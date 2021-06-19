1a745ba1851cf01695a00560a3c2d850aaf66e1a
Achillies-design <sweet_milan_j_parmar@yahoo.com>
2021-06-19 14:37:53 +0530

    commit

--- lib/screen/summary_screen.dart
+++ lib/screen/summary_screen.dart
@@ -85,6 +85,9 @@ class _SumaryScreenState extends State<SumaryScreen> {
           StaggeredTile.extent(2, 300.0),
           StaggeredTile.extent(2, 150.0),
           StaggeredTile.extent(2, 150.0),
+          StaggeredTile.extent(2, 150.0),
+          StaggeredTile.extent(2, 150.0),
+          StaggeredTile.extent(2, 150.0),
         ],
       )),
     );
